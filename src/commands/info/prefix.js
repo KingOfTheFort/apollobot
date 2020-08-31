cfb523dfd5d00e06d55b7f33c0def0774ff1cd76
Landon <dinotamer10@gmail.com>
2020-08-31 10:55:23 -0500

    Commit one, added 8/31/2020 existing Apollo Bot

---
+++ src/commands/info/prefix.js
@@ -0,0 +1,26 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class PrefixCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'prefix',
+      aliases: ['pre'],
+      usage: 'prefix',
+      description: 'Fetches Apollo\'s current prefix.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id); // Get prefix
+    const embed = new MessageEmbed()
+      .setTitle('Apollo\'s Prefix')
+      .setThumbnail('https://cdn.discordapp.com/icons/740991579342503936/9fedf3b4af21176bb9b400150b33ff14.webp?size=2048')
+      .addField('Prefix', `\`${prefix}\``, true)
+      .addField('Example', `\`${prefix}ping\``, true)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
---
+++ src/events/message.js
@@ -0,0 +1,62 @@
+const { MessageEmbed } = require('discord.js');
+const { oneLine } = require('common-tags');
+
+module.exports = (client, message) => {
+  if (message.channel.type === 'dm' || !message.channel.viewable || message.author.bot) return;
+
+  // Get disabled commands
+  let disabledCommands = client.db.settings.selectDisabledCommands.pluck().get(message.guild.id) || [];
+  if (typeof(disabledCommands) === 'string') disabledCommands = disabledCommands.split(' ');
+  
+  // Get points
+  const { point_tracking: pointTracking, message_points: messagePoints, command_points: commandPoints } = 
+    client.db.settings.selectPoints.get(message.guild.id);
+
+  // Command handler
+  const prefix = client.db.settings.selectPrefix.pluck().get(message.guild.id);
+  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})\\s*`);
+
+  if (prefixRegex.test(message.content)) {
+
+    const [, match] = message.content.match(prefixRegex);
+    const args = message.content.slice(match.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    let command = client.commands.get(cmd) || client.aliases.get(cmd); // If command not found, check aliases
+    if (command && !disabledCommands.includes(command.name)) {
+
+      // Check permissions
+      const permission = command.checkPermissions(message);
+      if (permission) {
+
+        // Update points with commandPoints value
+        if (pointTracking)
+          client.db.users.updatePoints.run({ points: commandPoints }, message.author.id, message.guild.id);
+        message.command = true; // Add flag for messageUpdate event
+        return command.run(message, args); // Run command
+      }
+    } else if ( 
+      (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) &&
+      message.channel.permissionsFor(message.guild.me).has(['SEND_MESSAGES', 'EMBED_LINKS'])
+    ) {
+      const embed = new MessageEmbed()
+        .setTitle('Apollo/Tagged')
+        .setThumbnail('https://cdn.discordapp.com/icons/740991579342503936/9fedf3b4af21176bb9b400150b33ff14.webp?size=2048')
+        .setDescription(`You can see everything I can do by using the \`${prefix}help\` command.`)
+        .addField('Invite Apollo', oneLine`
+          You can add me to your server by clicking 
+          [here](https://discord.com/oauth2/authorize?client_id=749270794076422144&permissions=8&scope=bot).
+        `)
+        .addField('Support', oneLine`
+          If you have any questions, DM kingofthefort#8622 as also said below.
+          **Thank you for choosing Apollo.*
+        `)
+        .setFooter('Apollo - Message kingofthefort#8622 for assistance.')
+        .setColor(message.guild.me.displayHexColor);
+      message.channel.send(embed);
+    }
+  }
+
+  // Update points with messagePoints value
+  if (pointTracking) client.db.users.updatePoints.run({ points: messagePoints }, message.author.id, message.guild.id);
+};
+
