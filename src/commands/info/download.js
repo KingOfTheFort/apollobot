d2c56e8f44c582f5612cc42fdd3f19f3db987638
SuperS123 <57376381+SuperS123@users.noreply.github.com>
2020-10-16 08:52:49 -0400

    Create download.js

---
+++ src/commands/info/download.js
@@ -0,0 +1,16 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class BirdCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'download',
+      usage: 'dl',
+      description: 'Send the download FAQ message.',
+      type: client.types.INFO
+    });
+  }
+  async run(message) {
+    message.channel.send('Apollo Client hasn\'t been released yet, but you can build it yourself using the source code at https://apolloclient.net/')
+  }
+};
